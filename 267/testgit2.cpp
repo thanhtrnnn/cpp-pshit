0db225d295f2b8d396579f6855cd305bbfa6d1c0
thanhtrnnn <thanhbx6a@gmail.com>
2024-03-28 11:19:44 +0700

    kmm

---
+++ 267/testgit2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define forloop(i, a, b) for (int i = (a); i < (b); i++)
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int t; cin >> t;
+    while(t--)
+    {
+        int n; cin >> n;
+        vector<int> a(n);
+        forloop(i, 0, n) cin >> a[i];
+        ặioajwdka[pưk]
+    }
+}
